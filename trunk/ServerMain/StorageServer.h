b9f6da26484d5b5b926aeb2d35e06bd25bc6112c
angeloprude92 <angeloprude92@663713fb-595e-4411-9141-cc0d7c17c1fc>
2016-04-29 15:08:53 +0000

    server main class

--- trunk/ServerMain/StorageServer.h
+++ trunk/ServerMain/StorageServer.h
@@ -10,8 +10,12 @@
 #include <string>
 
 #include "ServerSocket.h"
+#include "Executor.h"
 
-public class TStorageServer : public IServerSockController{
+////////////////////////////////////
+//        TStorageServer	      //
+////////////////////////////////////
+public class TStorageServer : public IServerSockController, public IServerExecutorController{
 private:
 	int fServerPort = -1;
 
@@ -19,18 +23,35 @@ private:
 	gcroot<IManagedServerSockController^> fCallbackObj = nullptr;
 	
 	TServerSockController* fSockController = nullptr;
+	IBaseExecutorController* fExeController = nullptr;
+	TMessageExecutor* fExecutor = nullptr;
 
 public:
-	TStorageServer(int AServerPort, IManagedServerSockController^ aCallback);
+	TStorageServer(int AServerPort, IManagedServerSockController^ aCallbackObj);
 	~TStorageServer();
 
-	void StartServer();
+	void startServer();
 
+	void onServerLog(string aClassName, string aFuncName, string aMsg) override;
+	void onServerWarning(string aClassName, string aFuncName, string aMsg) override;
+	void onServerError(string aClassName, string aFuncName, string aMsg) override;
+	void onServerCriticalError(string aClassName, string aFuncName, string aMsg) override; 
 	void onServerSockCreate() override;
-	void onServerSockLog(string aClassName, string aFuncName, string aMsg) override;
-	void onServerSockError(string aClassName, string aFuncName, string aMsg) override;
-	void onServerSockCriticalError(string aClassName, string aFuncName, string aMsg) override; 
 	void onServerSockAccept(TConnectionHandle aConnection) override;
-	void onServerSockRead(TConnectionHandle aConnection, string aMsg) override;
+	void onServerSockRead(TConnectionHandle aConnection, string_ptr aMsg) override;
+	void onServerSockWrite() override;
+
+	TMessageContainer getMessageToProcess() override;
+	void enqueueMessageToSend(TMessageContainer aMsg) override;
+	void processRegistrationRequest(TConnectionHandle aConnection, TUserRegistrReqMessage aMsg) override;
+	void processUpdateStartRequest(TConnectionHandle aConnection, TUpdateStartReqMessage aMsg) override;
+	void processAddNewFile(TConnectionHandle aConnection, TAddNewFileMessage aMsg) override;
+	void processUpdateFile(TConnectionHandle aConnection, TUpdateFileMessage aMsg) override;
+	void processRemoveFile(TConnectionHandle aConnection, TRemoveFileMessage aMsg) override;
+	void processUpdateStopRequest(TConnectionHandle aConnection, TUpdateStopReqMessage aMsg) override;
+	void processGetVersions(TConnectionHandle aConnection, TGetVersionsReqMessage aMsg) override;
+	void processRestoreVersion(TConnectionHandle aConnection, TRestoreVerReqMessage aMsg) override;
+	void processRestoreFileAck(TConnectionHandle aConnection, TRestoreFileAckMessage aMsg) override;
+	void processPingRequest(TConnectionHandle aConnection, TPingReqMessage aMsg) override;
 };
 
