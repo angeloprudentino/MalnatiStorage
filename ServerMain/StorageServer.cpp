05e285ca95fe96aae749370e58bdbe40512e08e6
angeloprude92 <12662466+angeloprudentino@users.noreply.github.com>
2016-04-29 15:08:53 +0000

    server main class

--- ServerMain/StorageServer.cpp
+++ ServerMain/StorageServer.cpp
@@ -9,54 +9,144 @@
 #include "StorageServer.h"
 #include "Utility.h"
 
-TStorageServer::TStorageServer(int AServerPort, IManagedServerSockController^ aCallback){
+////////////////////////////////////
+//        TStorageServer	      //
+////////////////////////////////////
+#pragma region "TStorageServer"
+TStorageServer::TStorageServer(int AServerPort, IManagedServerSockController^ aCallbackObj){
+	initCrypto();
+
 	this->fServerPort = AServerPort;
-	this->fCallbackObj = aCallback;
-	onServerSockLog("TStorageServer", "constructor", "creating TServerSockController object...");
+	this->fCallbackObj = aCallbackObj;
+	
+	this->onServerLog("TStorageServer", "constructor", "creating TServerSockController object...");
+
 	this->fSockController = new TServerSockController(this->fServerPort, this);
-	onServerSockLog("TStorageServer", "constructor", "TServerSockController object created");
+	if (!(this->fExeController = dynamic_cast<IBaseExecutorController*>(this->fSockController))){
+		this->fExeController = nullptr;
+		this->onServerWarning("TStorageServer", "constructor", "socket controller does not implement the IBaseExecutorController interface!");
+	}
+
+	this->onServerLog("TStorageServer", "constructor", "TServerSockController object created");
 }
 
 TStorageServer::~TStorageServer(){
 	if (this->fSockController != nullptr){
-		onServerSockLog("TStorageServer", "destructor", "deleting TServerSockController object...");
+		this->onServerLog("TStorageServer", "destructor", "deleting TServerSockController object...");
 		delete this->fSockController;
-		onServerSockLog("TStorageServer", "destructor", "TServerSockController object deleted");
+		this->onServerLog("TStorageServer", "destructor", "TServerSockController object deleted");
+		this->fSockController = nullptr;
+		this->fExeController = nullptr;
 	}
-	this->fSockController = nullptr;
+
+	if (this->fExecutor != nullptr){
+		this->onServerLog("TStorageServer", "destructor", "deleting TMessageExecutor object...");
+		delete this->fExecutor;
+		this->onServerLog("TStorageServer", "destructor", "TMessageExecutor object deleted");
+		this->fExecutor = nullptr;
+	}
+
 	this->fCallbackObj = nullptr;
 }
 
-void TStorageServer::StartServer(){
-	onServerSockLog("TStorageServer", "StartServer", "starting the internal server socket...");
-	this->fSockController->StartSocket();
+void TStorageServer::startServer(){
+	this->onServerLog("TStorageServer", "StartServer", "starting the internal server socket...");
+	this->fSockController->startSocket();
+	this->onServerLog("TStorageServer", "StartServer", "internal server socket started");
+
+	this->onServerLog("TStorageServer", "StartServer", "creating TMessageExecutor object...");
+	this->fExecutor = new TMessageExecutor(this);
+	this->onServerLog("TStorageServer", "StartServer", "TMessageExecutor object created");
 }
 
+#pragma region "IServerBaseController implementation"
+void TStorageServer::onServerLog(string aClassName, string aFuncName, string aMsg){
+	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
+		this->fCallbackObj->onServerLog(aClassName, aFuncName, aMsg);
+}
+
+void TStorageServer::onServerWarning(string aClassName, string aFuncName, string aMsg){
+	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
+		this->fCallbackObj->onServerWarning(aClassName, aFuncName, aMsg);
+}
+
+void TStorageServer::onServerError(string aClassName, string aFuncName, string aMsg){
+	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
+		this->fCallbackObj->onServerError(aClassName, aFuncName, aMsg);
+}
+
+void TStorageServer::onServerCriticalError(string aClassName, string aFuncName, string aMsg){
+	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
+		this->fCallbackObj->onServerError(aClassName, aFuncName, aMsg);
+}
+#pragma endregion
+
+#pragma region "IServerSockController implementation"
 void TStorageServer::onServerSockCreate(){
-	onServerSockLog("TStorageServer", "onServerSockCreate", "internal server socket correctly started");
+	this->onServerLog("TStorageServer", "onServerSockCreate", "internal server socket correctly started");
 	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
 		this->fCallbackObj->onServerSockCreate();
 }
 
-void TStorageServer::onServerSockLog(string aClassName, string aFuncName, string aMsg){
-	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
-		this->fCallbackObj->onServerSockLog(aClassName, aFuncName, aMsg);
+void TStorageServer::onServerSockAccept(TConnectionHandle aConnection){}
+
+void TStorageServer::onServerSockRead(TConnectionHandle aConnection, string_ptr aMsg){}
+
+void TStorageServer::onServerSockWrite(){}
+#pragma endregion
+
+#pragma region "IServerExecutorController implementation"
+TMessageContainer TStorageServer::getMessageToProcess(){
+	TMessageContainer result;
+	if (this->fExeController != nullptr)
+		result = this->fExeController->getMessageToProcess();
+
+	return result;
 }
 
-void TStorageServer::onServerSockError(string aClassName, string aFuncName, string aMsg){
-	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
-		this->fCallbackObj->onServerSockError(aClassName, aFuncName, aMsg);
+void TStorageServer::enqueueMessageToSend(TMessageContainer aMsg){
+	if (this->fExeController != nullptr)
+		this->fExeController->enqueueMessageToSend(aMsg);
 }
 
-void TStorageServer::onServerSockCriticalError(string aClassName, string aFuncName, string aMsg){
-	if (!System::Object::ReferenceEquals(this->fCallbackObj, nullptr))
-		this->fCallbackObj->onServerSockError(aClassName, aFuncName, aMsg);
-}
-
-void TStorageServer::onServerSockAccept(TConnectionHandle aConnection){
+void TStorageServer::processRegistrationRequest(TConnectionHandle aConnection, TUserRegistrReqMessage aMsg){
 
 }
 
-void TStorageServer::onServerSockRead(TConnectionHandle aConnection, string aMsg){
+void TStorageServer::processUpdateStartRequest(TConnectionHandle aConnection, TUpdateStartReqMessage aMsg){
 
 }
+
+void TStorageServer::processAddNewFile(TConnectionHandle aConnection, TAddNewFileMessage aMsg){
+
+}
+
+void TStorageServer::processUpdateFile(TConnectionHandle aConnection, TUpdateFileMessage aMsg){
+
+}
+
+void TStorageServer::processRemoveFile(TConnectionHandle aConnection, TRemoveFileMessage aMsg){
+
+}
+
+void TStorageServer::processUpdateStopRequest(TConnectionHandle aConnection, TUpdateStopReqMessage aMsg){
+
+}
+
+void TStorageServer::processGetVersions(TConnectionHandle aConnection, TGetVersionsReqMessage aMsg){
+
+}
+
+void TStorageServer::processRestoreVersion(TConnectionHandle aConnection, TRestoreVerReqMessage aMsg){
+
+}
+
+void TStorageServer::processRestoreFileAck(TConnectionHandle aConnection, TRestoreFileAckMessage aMsg){
+
+}
+
+void TStorageServer::processPingRequest(TConnectionHandle aConnection, TPingReqMessage aMsg){
+
+}
+#pragma endregion
+#pragma endregion
