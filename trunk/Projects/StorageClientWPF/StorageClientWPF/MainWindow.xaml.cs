410422705032b2736a9fcbf24372d124412a65e5
angeloprude92 <angeloprude92@663713fb-595e-4411-9141-cc0d7c17c1fc>
2016-09-15 17:41:00 +0000

    WPF project

---
+++ trunk/Projects/StorageClientWPF/StorageClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StorageClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window//, StorageClientController
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //StorageClientCore core = new StorageClientCore(this);
+            //Version v = new Version()
+            //core.test();
+            btn.Visibility = Visibility.Collapsed;
+        }
+    }
+}
