197e2ed9b9c15f3a70fec19cf46b99deb3ed85da
danyusaf92 <danyusaf92@663713fb-595e-4411-9141-cc0d7c17c1fc>
2016-09-15 19:34:12 +0000

    Inizio interfaccia grafica

--- trunk/Projects/StorageClientWPF/StorageClientWPF/MainWindow.xaml.cs
+++ trunk/Projects/StorageClientWPF/StorageClientWPF/MainWindow.xaml.cs
@@ -20,9 +20,20 @@ namespace StorageClientWPF
     /// </summary>
     public partial class MainWindow : Window//, StorageClientController
     {
+        private string username;
+        private string password;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.user.Visibility = Visibility.Collapsed;
+            this.user_label.Visibility = Visibility.Collapsed;
+            this.pass.Visibility = Visibility.Collapsed;
+            this.pass_label.Visibility = Visibility.Collapsed;
+            this.LoginButton.Visibility = Visibility.Collapsed;
+            this.second_pass_label.Visibility = Visibility.Collapsed;
+            this.repeat_pass.Visibility = Visibility.Collapsed;
+            this.Register_button.Visibility = Visibility.Collapsed;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,7 +41,45 @@ namespace StorageClientWPF
             //StorageClientCore core = new StorageClientCore(this);
             //Version v = new Version()
             //core.test();
-            btn.Visibility = Visibility.Collapsed;
+           // btn.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Click on Register
+            Login.Visibility = Visibility.Collapsed;
+            Register.Visibility = Visibility.Collapsed;
+            this.user.Visibility = Visibility.Visible;
+            this.user_label.Visibility = Visibility.Visible;
+            this.pass.Visibility = Visibility.Visible;
+            this.pass_label.Visibility = Visibility.Visible;
+            this.repeat_pass.Visibility = Visibility.Visible;
+            this.second_pass_label.Visibility = Visibility.Visible;
+            this.Register_button.Visibility = Visibility.Visible;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            //Click on Login
+            Login.Visibility = Visibility.Collapsed;
+            Register.Visibility = Visibility.Collapsed;
+            this.user.Visibility = Visibility.Visible;
+            this.user_label.Visibility = Visibility.Visible;
+            this.pass.Visibility = Visibility.Visible;
+            this.pass_label.Visibility = Visibility.Visible;
+            this.LoginButton.Visibility = Visibility.Visible;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.username = this.user.Text;
+            this.password = this.pass.Password;
+
+            if (this.password.CompareTo("Pippo")!=0)
+            {
+                MessageBox.Show("Invalid Credentials");
+            }
+            this.text.Text = "user:" + this.username + " pass:" + this.password;
         }
     }
 }
